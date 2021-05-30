751bfad59d23034709046ecd95dc0ba3f7fa9ac4
Alex <alexkhlebodarov@mail.ru>
2021-05-30 20:34:28 +0300

    Drag'n Drop feature

--- script.js
+++ script.js
@@ -28,9 +28,20 @@ function addTodoItem(todoItem) {
             todoEl.classList.add('done');
         }
 
-        todoEl.innerText = todoInput;
+        todoEl.setAttribute('draggable', true);
         todoContainer.appendChild(todoEl);
+        todoEl.innerText = todoInput;
         input.value = "";
+
+        todoEl.addEventListener('dragstart', (e) => {
+            todoEl.classList.add('todo__item_dragging');
+            let img = new Image();
+            e.dataTransfer.setDragImage(img, 5000, 5000);
+        })
+
+        todoEl.addEventListener('dragend', () => {
+            todoEl.classList.remove('todo__item_dragging');
+        })
 
         todoEl.addEventListener('click', () => {
             todoEl.classList.toggle('done');
@@ -46,10 +57,12 @@ function addTodoItem(todoItem) {
     }
 }
 
+//Update LocalStorage
+
 function updateLS() {
     const todoItems = document.querySelectorAll("li");
     const todos = [];
-    todoItems.forEach(item => {
+    todoItems.forEach((item, i) => {
         todos.push({
             content: item.innerText,
             done: item.classList.contains('done')
@@ -58,6 +71,8 @@ function updateLS() {
     localStorage.setItem('todos', JSON.stringify(todos));
 }
 
+
+//Options
 
 remove.addEventListener('click', () => {
     const todoItems = document.querySelectorAll("li");
@@ -85,3 +100,31 @@ todoInfo.addEventListener('mouseenter', () => {
 todoInfo.addEventListener('mouseout', () => {
     tooltip.classList.remove('todoApp-tooltip_active');
 })
+
+
+//Drag'n Drop
+todoContainer.addEventListener('dragover', (e) => {
+    e.preventDefault()
+    const afterElement = getDragAfterElement(todoContainer, e.clientY)
+    const draggable = document.querySelector('.todo__item_dragging')
+    if (afterElement == null) {
+        todoContainer.appendChild(draggable)
+    } else {
+        todoContainer.insertBefore(draggable, afterElement)
+    }
+})
+
+function getDragAfterElement(todoContainer, y) {
+    const draggableElements = [...todoContainer.querySelectorAll('li:not(todo__item_dragging)')];
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect()
+        const offset = y - box.top - box.height / 2
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child }
+        } else {
+            return closest;
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+}
+
